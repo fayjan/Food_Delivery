96a7cbf5ba7ec1322e0aa38e1df4d1f4a4bdccc3
fayjan <137094889+fayjan@users.noreply.github.com>
2024-09-28 17:36:16 +0530

    Home page Created with menu list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './Components/Pages/Home/Home';
+import Cart from './Components/Pages/Cart/Cart';
+import PlaceOrder from './Components/Pages/PlaceOrder/PlaceOrder';
 
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element ={<Home/>} /> 
+        <Route path='/cart' element ={<Cart/>} /> 
+        <Route path='/order' element ={<PlaceOrder/>} /> 
+      </Routes> 
     </div>
   )
 }
